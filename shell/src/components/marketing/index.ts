07cc56d1cccfd62b286673564a48a04d36a89a1a
ResumeCraft AI <dev@resumecraft.ai>
2026-02-01 10:56:37 +0100

    Fix: Remove broken marketing content, simplify homepage - app stable

--- shell/src/components/marketing/index.ts
+++
@@ -1,7 +0,0 @@
-export { ArticleLayout } from './ArticleLayout';
-export { AuthorBox } from './AuthorBox';
-export { CTASection } from './CTASection';
-export { HeroImage } from './HeroImage';
-export { RelatedArticles } from './RelatedArticles';
-export { SEOHead } from './SEOHead';
-export { TemplateShowcase } from './TemplateShowcase';
